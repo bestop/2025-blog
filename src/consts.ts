c59d1a55f3064c4317f42f269c2eba891ff68a02
bestop <bestopjoe@gmail.com>
2025-11-28 14:08:21 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -7,7 +7,7 @@ export const CARD_SPACING_SM = 24
  * GitHub 仓库配置
  */
 export const GITHUB_CONFIG = {
-	OWNER: process.env.NEXT_PUBLIC_GITHUB_OWNER || 'Joe',
+	OWNER: process.env.NEXT_PUBLIC_GITHUB_OWNER || 'bestop',
 	REPO: process.env.NEXT_PUBLIC_GITHUB_REPO || '2025-blog',
 	BRANCH: process.env.NEXT_PUBLIC_GITHUB_BRANCH || 'main',
 	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '2371838'
